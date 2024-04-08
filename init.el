ba0eaa2aa62d2691219ff6fc830b2b843f64e1ff
Bryce MacInnis <Bryce.MacInnis@quest.com>
2024-04-08 19:36:25 -0300

    Updates emacs to the latest and greatest version of my config

--- init.el
+++ init.el
@@ -3,16 +3,23 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(ef-arbutus))
+ '(custom-enabled-themes '(doom-ayu-dark))
  '(custom-safe-themes
-   '("91d9dd2bc02a71a018738c815ba64be0ac0ec12815324fb28733c95d414f3f00" "e8915dac580af7a4a57bd38a0638a5a7714b47714529b1a66bd19e7aa155818d" "95b0bc7b8687101335ebbf770828b641f2befdcf6d3c192243a251ce72ab1692" "01aef17f41edea53c665cb57320bd80393761f836be5ab0bd53292afc94bd14d" "b0b29575dda28e1948e31f40e7693fa8483d7a9b1b7aff069fa8a1b7193cd1ab" "7ca04d620046f5807d0740f265844d45e53b864138c246f48f663bea8fba5c5d" "ae7aede615d12f126a7b6b0d8c1e0af2af1ab63fb21d80a130a5f701fdbb23fe" "df42062cdd672acecac9b5a1229c45f74c0cc2bc0362f9ad41054af6ac355021" "7a259016d8fe61ef450895a01d842c4353dabc873ee46e250c933995f7f004cf" "a6a979c8b7ccb1d4536f4fa74a6e47674a3ce65feea3fecdf1d9dc448fac47e0" "7dc1c6210efe106a8c6cd47009a2ffd0069826b550dda379e8e4ef6105384cba" "704f75d46620d87bb246e2ec1abb129437764b0e84ac0fff6b968311cc046918" "c73ce0e82bfbf5f186756001eeaaf26f0b461851a8b421d1ff5ec35e10f300ed" "4c326abbf8b85c85e114691d3892cbbfe889b2b064dadd284cf5eccca3eecbff" "205bb9accecaf0ae5e8cb5f09773be4f1175aca71322ba4fd44b539cd48463b6" "28d5b595e4724ad44b52eb1ab9bd6fbf1cca747109e2dc5ff4073a07d1b50946" "67a0f6cdf628610987cf7f0d38f0ae68ed3a5ab6816ff3da5f66d7b09d03f470" "91fed8cade0d85d1f5a5621e72ac6e720945be79493465a79f020e673f7e2d24" "65af8e8d704bcd9745a4f191db756995de6b1fdd15cf2eb41befaae75f7b045d" "c49f79bfea991ed384cc0dc33328c8d812c413bf27cff0bc24ad58af2cdbccb4" "65809263a533c5151d522570b419f1a653bfd8fb97e85166cf4278e38c39e00e" "4de156ea6aa06640e3d16da41dfbdee85aea2fb969f620e601a982c6e237b396" "e546768f3da4b394adc8c460106a7d220af130a3a2a0518d265c832d015a4385" "38cb7d03fec58c860d5e92ee756aa80bdb2d78ef7ac726317e78c5c844e04f3a" "a73cd17e5b490a5558a0ca13186cccb85ff74b2160f77e65ff36024bc66089b4" "ca934a76aae4ff950288e082be75a68eb7bac6e8d3dd58b28649993540412ed6" "7d10494665024176a90895ff7836a8e810d9549a9872c17db8871900add93d5c" "46a843168cc83b28b740735516e6eea4f97769d848c79b5acab32f7a278f793a" "8ea6a46120abb34bf6a664b76d78014e0dd1f2b740a0976ec41313612421712f" "5f92b9fc442528b6f106eaefa18bb5e7bfa0d737164e18f1214410fef2a6678d" "7819a530936ad2204a35340498da6341f66d816a92b8c71af138f73814b269ec" "5e05db868f138062a3aedcccefe623eee18ec703ae25d4e5aebd65f892ac5bcc" "73c55f5fd22b6fd44f1979b6374ca7cc0a1614ee8ca5d4f1366a0f67da255627" default))
+   '("81f53ee9ddd3f8559f94c127c9327d578e264c574cda7c6d9daddaec226f87bb" "e4a702e262c3e3501dfe25091621fe12cd63c7845221687e36a79e17cf3a67e0" "1f292969fc19ba45fbc6542ed54e58ab5ad3dbe41b70d8cb2d1f85c22d07e518" "013728cb445c73763d13e39c0e3fd52c06eefe3fbd173a766bfd29c6d040f100" "7e377879cbd60c66b88e51fad480b3ab18d60847f31c435f15f5df18bdb18184" "e1f4f0158cd5a01a9d96f1f7cdcca8d6724d7d33267623cc433fe1c196848554" "4594d6b9753691142f02e67b8eb0fda7d12f6cc9f1299a49b819312d6addad1d" "77fff78cc13a2ff41ad0a8ba2f09e8efd3c7e16be20725606c095f9a19c24d3d" "6e33d3dd48bc8ed38fd501e84067d3c74dfabbfc6d345a92e24f39473096da3f" "7964b513f8a2bb14803e717e0ac0123f100fb92160dcf4a467f530868ebaae3e" "ffafb0e9f63935183713b204c11d22225008559fa62133a69848835f4f4a758c" "10e5d4cc0f67ed5cafac0f4252093d2119ee8b8cb449e7053273453c1a1eb7cc" "f64189544da6f16bab285747d04a92bd57c7e7813d8c24c30f382f087d460a33" "0c83e0b50946e39e237769ad368a08f2cd1c854ccbcd1a01d39fdce4d6f86478" "93011fe35859772a6766df8a4be817add8bfe105246173206478a0706f88b33d" "2078837f21ac3b0cc84167306fa1058e3199bbd12b6d5b56e3777a4125ff6851" "4b6cc3b60871e2f4f9a026a5c86df27905fb1b0e96277ff18a76a39ca53b82e1" "56044c5a9cc45b6ec45c0eb28df100d3f0a576f18eef33ff8ff5d32bac2d9700" "e8ceeba381ba723b59a9abc4961f41583112fc7dc0e886d9fc36fa1dc37b4079" "5f128efd37c6a87cd4ad8e8b7f2afaba425425524a68133ac0efd87291d05874" "9d5124bef86c2348d7d4774ca384ae7b6027ff7f6eb3c401378e298ce605f83a" "2b501400e19b1dd09d8b3708cefcb5227fda580754051a24e8abf3aff0601f87" "6a5584ee8de384f2d8b1a1c30ed5b8af1d00adcbdcd70ba1967898c265878acf" "88f7ee5594021c60a4a6a1c275614103de8c1435d6d08cc58882f920e0cec65e" "9f297216c88ca3f47e5f10f8bd884ab24ac5bc9d884f0f23589b0a46a608fe14" "b754d3a03c34cfba9ad7991380d26984ebd0761925773530e24d8dd8b6894738" "c5878086e65614424a84ad5c758b07e9edcf4c513e08a1c5b1533f313d1b17f1" "9013233028d9798f901e5e8efb31841c24c12444d3b6e92580080505d56fd392" "a6920ee8b55c441ada9a19a44e9048be3bfb1338d06fc41bce3819ac22e4b5a1" "00cec71d41047ebabeb310a325c365d5bc4b7fab0a681a2a108d32fb161b4006" "34cf3305b35e3a8132a0b1bdf2c67623bc2cb05b125f8d7d26bd51fd16d547ec" "7c28419e963b04bf7ad14f3d8f6655c078de75e4944843ef9522dbecfcd8717d" "571661a9d205cb32dfed5566019ad54f5bb3415d2d88f7ea1d00c7c794e70a36" "8d3ef5ff6273f2a552152c7febc40eabca26bae05bd12bc85062e2dc224cde9a" "b54376ec363568656d54578d28b95382854f62b74c32077821fdfd604268616a" "3fe1ebb870cc8a28e69763dde7b08c0f6b7e71cc310ffc3394622e5df6e4f0da" "f5f80dd6588e59cfc3ce2f11568ff8296717a938edd448a947f9823a4e282b66" "a9abd706a4183711ffcca0d6da3808ec0f59be0e8336868669dc3b10381afb6f" "8d8207a39e18e2cc95ebddf62f841442d36fcba01a2a9451773d4ed30b632443" "37b6695bae243145fa2dfb41440c204cd22833c25cd1993b0f258905b9e65577" "be84a2e5c70f991051d4aaf0f049fa11c172e5d784727e0b525565bb1533ec78" "b9761a2e568bee658e0ff723dd620d844172943eb5ec4053e2b199c59e0bcc22" "9d29a302302cce971d988eb51bd17c1d2be6cd68305710446f658958c0640f68" "2721b06afaf1769ef63f942bf3e977f208f517b187f2526f0e57c1bd4a000350" "da75eceab6bea9298e04ce5b4b07349f8c02da305734f7c0c8c6af7b5eaa9738" "f053f92735d6d238461da8512b9c071a5ce3b9d972501f7a5e6682a90bf29725" "ff24d14f5f7d355f47d53fd016565ed128bf3af30eb7ce8cae307ee4fe7f3fd0" "df6dfd55673f40364b1970440f0b0cb8ba7149282cf415b81aaad2d98b0f0290" "f4d1b183465f2d29b7a2e9dbe87ccc20598e79738e5d29fc52ec8fb8c576fcfd" "4990532659bb6a285fee01ede3dfa1b1bdf302c5c3c8de9fad9b6bc63a9252f7" "c8b3d9364302b16318e0f231981e94cbe4806cb5cde5732c3e5c3e05e1472434" "38c0c668d8ac3841cb9608522ca116067177c92feeabc6f002a27249976d7434" "dd4582661a1c6b865a33b89312c97a13a3885dc95992e2e5fc57456b4c545176" "e3daa8f18440301f3e54f2093fe15f4fe951986a8628e98dcd781efbec7a46f2" "691d671429fa6c6d73098fc6ff05d4a14a323ea0a18787daeb93fde0e48ab18b" "a9eeab09d61fef94084a95f82557e147d9630fbbb82a837f971f83e66e21e5ad" "e14884c30d875c64f6a9cdd68fe87ef94385550cab4890182197b95d53a7cf40" "c1d5759fcb18b20fd95357dcd63ff90780283b14023422765d531330a3d3cec2" "32f22d075269daabc5e661299ca9a08716aa8cda7e85310b9625c434041916af" "dfb1c8b5bfa040b042b4ef660d0aab48ef2e89ee719a1f24a4629a0c5ed769e8" "02d422e5b99f54bd4516d4157060b874d14552fe613ea7047c4a5cfa1288cf4f" "8c7e832be864674c220f9a9361c851917a93f921fedb7717b1b5ece47690c098" "aec7b55f2a13307a55517fdf08438863d694550565dee23181d2ebd973ebd6b8" "7ec8fd456c0c117c99e3a3b16aaf09ed3fb91879f6601b1ea0eeaee9c6def5d9" "13096a9a6e75c7330c1bc500f30a8f4407bd618431c94aeab55c9855731a95e1" "6f96a9ece5fdd0d3e04daea6aa63e13be26b48717820aa7b5889c602764cf23a" "8b148cf8154d34917dfc794b5d0fe65f21e9155977a36a5985f89c09a9669aa0" "456697e914823ee45365b843c89fbc79191fdbaff471b29aad9dcbe0ee1d5641" "6f1f6a1a3cff62cc860ad6e787151b9b8599f4471d40ed746ea2819fcd184e1a" "d6b934330450d9de1112cbb7617eaf929244d192c4ffb1b9e6b63ad574784aad" "4ade6b630ba8cbab10703b27fd05bb43aaf8a3e5ba8c2dc1ea4a2de5f8d45882" "4e2e42e9306813763e2e62f115da71b485458a36e8b4c24e17a2168c45c9cf9d" "dccf4a8f1aaf5f24d2ab63af1aa75fd9d535c83377f8e26380162e888be0c6a9" "b5fd9c7429d52190235f2383e47d340d7ff769f141cd8f9e7a4629a81abc6b19" "014cb63097fc7dbda3edf53eb09802237961cbb4c9e9abd705f23b86511b0a69" "9f27d5afd6d78b40bf1290c10722818e0b90f141fc3758d3c2d284ccb565de15" "b216e9b72dc8c2b702e4fcfd3c0af2d73c87eba46fd4db824ddb50863447d6a9" "5efa59da0b446dd939749e86fdf414ef2b666f80243999633d9e2e4fd22fd37c" "ca47f7b222eb380e3035fb594d52032acd89dae0a49eac3da722a5cd3f485e3b" "df1ed4aa97d838117dbda6b2d84b70af924b0380486c380afb961ded8a41c386" "0664443859604a53d2257701f034459edf8eab3cc2be50c7d8ae36740fe35578" "0cb6dc28758f1173709003cc65b6ee8553297548c69e094385cc4c25f6357c5b" "50d388e646c2c19b0fa60a229e6536760f757863b0fc3c193405e0499446ecaf" "e8480a7c0fcd73c430111858550dfb91326df654ccbe038485ba87158320595d" "23841f54afd15fe850da8b37f2bb5813a873f53ed528024b0becb486ad8b01b3" "702d0136433ca65a7aaf7cc8366bd75e983fe02f6e572233230a528f25516f7e" "24cfd7c82a9278b966f21a659ce805d5b6399655c0a39de813fd0fb6ee8b2d5c" "7af2a6fcd1e743d165c58fd95d20b46c2d96d9873ab67fc9371bdc8fda463de7" "84beb6ca7529b52c13124cafd04f9c3763b68a5a73bad89a1e6b596e6157af90" "159a29ab0ec5ba4e2811eddd9756aa779b23467723dcbdd223929fbf2dde8954" "82f1e895a3fb1f4b99efc81e9d732c850f55653689e9492b4eb1be292b4826c3" "c6f78f6a0324b541082bfcc7e1672d1b760b147ec2a09a37dbd469235ced473b" "3bfdc037f4e076a4afe8060a7e663a75eb2f3a5bedf1c9902a5d5645fba08d56" "caf6d2a2d0536871f3e0c3427d5f7cb6b6474f65ecf1d825ed74d043f913c277" "ed1b7b4db911724b2767d4b6ad240f5f238a6c07e98fff8823debcfb2f7d820a" "6ccb6eb66c70661934a94f395d755a84f3306732271c55d41a501757e4c39fcb" "0d9028987183602b113c27e52e549b2eeb23fb29ebfc19ca0b6612fa57472066" "972e40d60ad5703cb92b7ec1da9e2cec068d8732063b8b42874e0391624e50a1" "515ebca406da3e759f073bf2e4c8a88f8e8979ad0fdaba65ebde2edafc3f928c" "943f5fc368a959c5858ff466401357da25b5991c0eeb396e865fd8071e8fe88e" "f42ea77c304f98f13ce4ed3b7b84d4a96c69e651757185ba26aa7059d3219051" "0c5d7ffa7cdf8b889342d2cd38f6ddd2cc716561048feca89b17fda56677d6b8" "694dbeb8f98dddfb603a2fe0c04101f3fe457ee49bf90a6a581271e7f9c580c8" "95e934b092694a2624adb653043d1dc016a6712fa27b788f9ff4dffb8ee08290" "7776ba149258df15039b1f0aba4b180d95069b2589bc7d6570a833f05fdf7b6d" "91d9dd2bc02a71a018738c815ba64be0ac0ec12815324fb28733c95d414f3f00" "e8915dac580af7a4a57bd38a0638a5a7714b47714529b1a66bd19e7aa155818d" "95b0bc7b8687101335ebbf770828b641f2befdcf6d3c192243a251ce72ab1692" "01aef17f41edea53c665cb57320bd80393761f836be5ab0bd53292afc94bd14d" "b0b29575dda28e1948e31f40e7693fa8483d7a9b1b7aff069fa8a1b7193cd1ab" "7ca04d620046f5807d0740f265844d45e53b864138c246f48f663bea8fba5c5d" "ae7aede615d12f126a7b6b0d8c1e0af2af1ab63fb21d80a130a5f701fdbb23fe" "df42062cdd672acecac9b5a1229c45f74c0cc2bc0362f9ad41054af6ac355021" "7a259016d8fe61ef450895a01d842c4353dabc873ee46e250c933995f7f004cf" "a6a979c8b7ccb1d4536f4fa74a6e47674a3ce65feea3fecdf1d9dc448fac47e0" "7dc1c6210efe106a8c6cd47009a2ffd0069826b550dda379e8e4ef6105384cba" "704f75d46620d87bb246e2ec1abb129437764b0e84ac0fff6b968311cc046918" "c73ce0e82bfbf5f186756001eeaaf26f0b461851a8b421d1ff5ec35e10f300ed" "4c326abbf8b85c85e114691d3892cbbfe889b2b064dadd284cf5eccca3eecbff" "205bb9accecaf0ae5e8cb5f09773be4f1175aca71322ba4fd44b539cd48463b6" "28d5b595e4724ad44b52eb1ab9bd6fbf1cca747109e2dc5ff4073a07d1b50946" "67a0f6cdf628610987cf7f0d38f0ae68ed3a5ab6816ff3da5f66d7b09d03f470" "91fed8cade0d85d1f5a5621e72ac6e720945be79493465a79f020e673f7e2d24" "65af8e8d704bcd9745a4f191db756995de6b1fdd15cf2eb41befaae75f7b045d" "c49f79bfea991ed384cc0dc33328c8d812c413bf27cff0bc24ad58af2cdbccb4" "65809263a533c5151d522570b419f1a653bfd8fb97e85166cf4278e38c39e00e" "4de156ea6aa06640e3d16da41dfbdee85aea2fb969f620e601a982c6e237b396" "e546768f3da4b394adc8c460106a7d220af130a3a2a0518d265c832d015a4385" "38cb7d03fec58c860d5e92ee756aa80bdb2d78ef7ac726317e78c5c844e04f3a" "a73cd17e5b490a5558a0ca13186cccb85ff74b2160f77e65ff36024bc66089b4" "ca934a76aae4ff950288e082be75a68eb7bac6e8d3dd58b28649993540412ed6" "7d10494665024176a90895ff7836a8e810d9549a9872c17db8871900add93d5c" "46a843168cc83b28b740735516e6eea4f97769d848c79b5acab32f7a278f793a" "8ea6a46120abb34bf6a664b76d78014e0dd1f2b740a0976ec41313612421712f" "5f92b9fc442528b6f106eaefa18bb5e7bfa0d737164e18f1214410fef2a6678d" "7819a530936ad2204a35340498da6341f66d816a92b8c71af138f73814b269ec" "5e05db868f138062a3aedcccefe623eee18ec703ae25d4e5aebd65f892ac5bcc" "73c55f5fd22b6fd44f1979b6374ca7cc0a1614ee8ca5d4f1366a0f67da255627" default))
  '(helm-minibuffer-history-key "M-p")
+ '(org-safe-remote-resources
+   '("\\`https://raw\\.githubusercontent\\.com/fniessen/org-html-themes/master/org/theme-readtheorg\\.setup\\'" "\\`https://github\\.com/fniessen/org-html-themes/blob/master/org/theme-readtheorg\\.setup\\'"))
  '(package-selected-packages
-   '(vs-light-theme vs-dark-theme monokai-theme ivy-rich magit evil-collection evil-leader counsel ef-themes use-package)))
+   '(babel font-setup fontaine doom-themes centered-window-mode centered-window dashboard treemacs spacious-padding all-the-icons doom-modeline golden-ratio chatgpt-shell chatgpt powershell terraform-mode projectile vs-light-theme vs-dark-theme monokai-theme ivy-rich magit evil-collection evil-leader counsel ef-themes use-package)))
 
 (require 'package)
-(add-to-list 'package-archives
-             '("melpa-stable" . "https://stable.melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+;; Comment/uncomment this line to enable MELPA Stable if desired.
+;; See `package-archive-priorities` and `package-pinned-packages`.
+;; Most users will not need or want to do this.
+;; (add-to-list 'package-archives
+;;              '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(package-initialize)
 
 ;; This is only needed once, near the top of the file
 (eval-when-compile
@@ -21,6 +28,9 @@
   (require 'use-package)))
 
 (setq inhibit-startup-message t)
+
+;; Include a manually installed powershell mode
+(add-to-list 'load-path "~/.emacs.d/packages/powershell.el")
 
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
@@ -31,14 +41,18 @@
 (setq cursor-type nil)  ; or nil for a box cursor, or bar for a vertical bar cursor
 (blink-cursor-mode 1) ; or 0 to disable cursor blinking
 
-(set-face-attribute 'default nil :family "Consolas" :height 110)
+(set-face-attribute 'default nil :family "Consolas" :height 130)
 
 ;; Display line numbers in every buffer
 (global-display-line-numbers-mode 1)
 
+(use-package ef-themes
+  :ensure t)
 (use-package magit
   :ensure t
   :bind ("C-x g" . magit-status))
+
+(setq evil-want-keybinding nil)
 
 (use-package evil-collection
   :ensure t
@@ -120,5 +134,116 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(fringe ((t :background "#000000")))
+ '(header-line ((t :box (:line-width 4 :color "#1a1a1a" :style nil))))
+ '(header-line-highlight ((t :box (:color "#d0d0d0"))))
+ '(keycast-key ((t)))
+ '(line-number ((t :background "#000000")))
+ '(mode-line ((t :box (:line-width 6 :color "#2a2a75" :style nil))))
+ '(mode-line-active ((t :box (:line-width 6 :color "#2a2a75" :style nil))))
+ '(mode-line-highlight ((t :box (:color "#d0d0d0"))))
+ '(mode-line-inactive ((t :box (:line-width 6 :color "#2b2b2b" :style nil))))
+ '(tab-bar-tab ((t :box (:line-width 4 :color "#000000" :style nil))))
+ '(tab-bar-tab-inactive ((t :box (:line-width 4 :color "#4b4b4b" :style nil))))
+ '(tab-line-tab ((t)))
+ '(tab-line-tab-active ((t)))
+ '(tab-line-tab-inactive ((t)))
+ '(vertical-border ((t :background "#000000" :foreground "#000000")))
+ '(window-divider ((t (:background "#000000" :foreground "#000000"))))
+ '(window-divider-first-pixel ((t (:background "#000000" :foreground "#000000"))))
+ '(window-divider-last-pixel ((t (:background "#000000" :foreground "#000000")))))
 
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 4)
+(setq indent-line-function 'insert-tab)
+
+(add-to-list 'load-path "~/.emacs.d/packages/powershell.el")
+(add-to-list 'auto-mode-alist '("\\.ps1\\'" . powershell-mode))
+
+
+(use-package all-the-icons
+  :ensure t
+  :if (display-graphic-p))
+
+(use-package doom-modeline
+  :ensure t
+  :init (doom-modeline-mode 1)
+  :config (column-number-mode 1)
+  :custom
+  (doom-modeline-height 8)
+  (doom-modeline-window-width-limit nil)
+  (doom-modeline-buffer-file-name-style 'truncate-with-project)
+  (doom-modeline-minor-modes nil)
+  (doom-modeline-enable-word-count t)
+  (doom-modeline-buffer-encoding nil)
+  (doom-modeline-buffer-modification-icon t)
+  (doom-modeline-env-python-executable "python")
+  (doom-modeline-time t)
+  (doom-modeline-vcs-max-length 50)
+  )
+
+(use-package spacious-padding
+  :ensure t
+  :hook (after-init . spacious-padding-mode))
+
+(use-package treemacs
+  :ensure t
+  :bind ("<f5>" . treemacs)
+  :custom
+  (treemacs-is-never-other-window t)
+  :hook
+  (treemacs-mode . treemacs-project-follow-mode))
+
+;; use-package with package.el:
+(use-package dashboard
+  :ensure t
+  :config
+  (dashboard-setup-startup-hook))
+
+(use-package solaire-mode
+  :ensure t
+  :hook (after-init . solaire-global-mode)
+  :config
+  (push '(treemacs-window-background-face . solaire-default-face) solaire-mode-remap-alist)
+  (push '(treemacs-hl-line-face . solaire-hl-line-face) solaire-mode-remap-alist))
+
+(use-package chatgpt-shell
+  :ensure t
+  :custom
+  ((chatgpt-shell-openai-key
+    (lambda ()
+      (auth-source-pass-get 'secret "openai-key")))))
+
+(setq chatgpt-shell-openai-chatgpt-shell-model-versionkey (getenv "OPENAI_API_KEY"))
+(setq chatgpt-shell-model-version "3.5t-16k-0613")
+
+(global-set-key (kbd "C-x p") 'chatgpt-shell)
+
+(use-package centered-window
+  :ensure t)
+
+(use-package fontaine
+  :ensure t
+  :config
+  (setopt fontaine-presets
+          '((regular
+             :default-height 115)
+            (small
+             :default-height 110)
+            (large
+             :default-weight regular
+             :default-height 180
+             :bold-weight extrabold)
+            (extra-large
+             :default-weight semilight
+             :default-height 210
+             :line-spacing 5
+             :bold-weight ultrabold)
+            (t                        ; our shared fallback properties
+             :default-family "Consolas")))
+  (fontaine-set-preset 'regular))
+
+(org-babel-do-load-languages
+ 'org-babel-load-languages '((python . t)
+                             (shell . t))
+ 
