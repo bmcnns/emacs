093b9499e7e20e1a94ae6fd2709689013f3e7497
Bryce MacInnis <Bryce.MacInnis@dal.ca>
2025-09-21 11:59:13 -0300

    Adds basic auto-complete

--- init.el
+++ init.el
@@ -1,3 +1,4 @@
+
 (require 'package)
 (setq package-archives
       '(("melpa" . "https://melpa.org/packages/")
@@ -731,3 +732,33 @@ Defaults: /tmp/bryce.ics and 129.173.67.123."
 
 (advice-add 'notmuch :before #'my/notmuch-sync-before)
 
+(use-package corfu
+  :ensure t
+  ;; Optional customizations
+  ;;:custom
+  ;; (corfu-cycle t)                ;; Enable cycling for `corfu-next/previous'
+  ;; (corfu-quit-at-boundary nil)   ;; Never quit at completion boundary
+  ;; (corfu-quit-no-match nil)      ;; Never quit, even if there is no match
+  ;; (corfu-preview-current nil)    ;; Disable current candidate preview
+  ;; (corfu-preselect 'prompt)      ;; Preselect the prompt
+  ;; (corfu-on-exact-match nil)     ;; Configure handling of exact matches
+
+  ;; Enable Corfu only for certain modes. See also `global-corfu-modes'.
+  ;; :hook ((prog-mode . corfu-mode)
+  ;;        (shell-mode . corfu-mode)
+  ;;        (eshell-mode . corfu-mode))
+
+  :init
+
+  ;; Recommended: Enable Corfu globally.  Recommended since many modes provide
+  ;; Capfs and Dabbrev can be used globally (M-/).  See also the customization
+  ;; variable `global-corfu-modes' to exclude certain modes.
+  (global-corfu-mode)
+
+  ;; Enable optional extension modes:
+  ;; (corfu-history-mode)
+  ;; (corfu-popupinfo-mode)
+  )
+
+(setq corfu-auto t
+      corfu-quit-no-match 'separator)
